59815ec8e232e44d2bb552bea5d77803d0a8a72a
Parasf12 <admin@parasf.com>
2024-06-25 21:39:42 +0530

    Update Form.tsx

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -73,7 +73,7 @@ export function Form() {
           errors={state.errors}
         />
         <ReCAPTCHA
-          sitekey="6Lfj9NYfAAAAAP8wPLtzrsSZeACIcGgwuEIRvbSg"
+          sitekey="6LfoNQEqAAAAABLDYLiw4jdEcoVXBrXDiLC6HO6R"
           onChange={(e) => {
             setIsHuman(true)
           }}
